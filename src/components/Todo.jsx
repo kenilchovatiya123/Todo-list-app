81ab55d5005c86109d9b92e291dee8b962fb6d44
Chovatiya kenil <chovatiyakenil123@gmail.com>
2024-12-20 17:24:53 +0530

    final update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -47,17 +47,19 @@ const Todo = () => {
   return (
     <div className='bg-neutral-100 h-screen'>
       <div className='items-center justify-center flex flex-col'>
-        <div className='bg-gray-200 text-black w-full sm:w-[460px] flex items-center justify-start py-6 px-8 gap-1 sm:gap-2 rounded-md'>
-          <img src="/src/assets/checklist.png" alt="" className='h-6 sm:h-7 items-center'/>
+        <div className='bg-gray-200 text-black w-full sm:w-[460px] flex items-center justify-start py-6 px-8 gap-1 sm:gap-2 mt-20'>
+          <img src="/src/assets/checklist.png" alt="" className='h-6 sm:h-7 items-center' />
           <h1 className='sm:text-2xl text-lg font-bold tracking-tight'>Task Tracker</h1>
         </div>
-        <div className='bg-gray-200 text-black w-full sm:w-[460px] flex mt-2 items-center justify-between py-6 px-8 gap-1 rounded-md'>
+
+        <div className='bg-gray-200 text-black w-full sm:w-[460px] flex mt-2 items-center justify-between py-6 px-8 gap-1'>
           <div className='flex items-center w-full'>
-            <input ref={inputRef} type="text" placeholder='Add your tasks' className='w-full flex-1 py-2 px-3 text-[14px] sm:text-[16px] outline-none' />
+            <input ref={inputRef} type="text" placeholder='Add your tasks' className='w-full flex-1 py-2 px-3 text-[14px] sm:text-[16px] outline-none capitalize' />
             <button onClick={add} className='bg-neutral-800 py-2 px-6 sm:px-8 text-white text-[14px] sm:text-[16px]'>ADD</button>
           </div>
+
         </div>
-        <div>
+        <div className='bg-gray-200 text-black w-full sm:w-[460px] px-8 py-4'>
           {todoList.map((item, index) => {
             return <TodoItem key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
           })}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import check from '../assets/checked.png'
+import uncheck from '../assets/unchecked.png'
+import delete_icon from '../assets/trash.png'
 
 const TodoItem = ({ text, id, isComplete, deleteTodo, toggle }) => {
     return (
-        <div className='flex items-center my-4 gap-2'>
-            <div onClick={() => { toggle(id) }} className='flex flex-1 items-center cursor-pointer'>
-                <i className='w-8 text-xl text-gray-600 hover:text-green-600'>
-                    <FontAwesomeIcon icon={isComplete ? faSquareCheck : faSquare} />
-                </i>
-                <p className={`text-[16px] font-[500] text-gray-700 ml-4 decoration-slate-600 ${isComplete ? "line-through" : ""}`}>
+        <div className='flex justify-between items-center py-1'>
+            <div onClick={() => { toggle(id) }} className='flex items-center w-full'>
+                <a href="#"><img src={isComplete ? check : uncheck} className='w-5' /></a>
+                <p className={`text-[14px] sm:text-[16px] capitalize font-normal text-neutral-800 ml-2 ${isComplete ? "line-through" : ""}`}>
                     {text}
                 </p>
             </div>
-
-            <i onClick={() => { deleteTodo(id) }} className='text-xl text-gray-600 cursor-pointer transition duration-200 hover:text-red-500'><FontAwesomeIcon icon={faTrash} /></i>
+            <div>
+                <a href="#" onClick={() => { deleteTodo(id) }}><img src={delete_icon} className='w-5' /></a>
+            </div>
         </div>
     )
 }
